db6d8f532fcb5f1c58f841c95460c52bfe19bdbd
Ramadhana <ramadhanaa44@gmail.com>
2024-04-18 11:16:32 +0700

    aplikasi bank transfer dengan runtime configuration

---
+++ modul8_1302220048/BankTransferConfig.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Json;
+
+
+namespace modul8_1302220048
+{
+     class BankTransferConfig
+    {
+        public string lang {  get; set; } 
+        public int threshold { get; set; }
+        public int low_fee {  get; set; } 
+        public int high_fee { get; set; }
+
+        public string methods { get; set; } 
+        public string en {  get; set; }
+        public string id { get; set; }
+
+
+
+     }
+
+    class MainBankTransferConfig
+    {
+        public BankTransferConfig dataConfig;
+
+        private string filePath = "C:\\semester 4\\konstruksi perangkat lunak\\bank_transfer_config.json";
+
+        public void ReadJSON()
+        {
+            string configJsonData = File.ReadAllText(filePath);
+            this.dataConfig = JsonSerializer.Deserialize<BankTransferConfig>(configJsonData);
+        }
+
+        public void WriteJSON()
+        {
+            JsonSerializerOptions options = new JsonSerializerOptions()
+            {
+                WriteIndented = true
+            };
+
+            string jsonString = JsonSerializer.Serialize(dataConfig, options);
+            File.WriteAllText(filePath, jsonString);
+        }
+
+        public void setDefault()
+        {
+            dataConfig = new BankTransferConfig();
+            dataConfig.lang = "en";
+            dataConfig.threshold = 25000000;
+            dataConfig.low_fee = 6500;
+            dataConfig.high_fee = 15000;
+            dataConfig.methods = "“RTO", "(real-time)”", "“SKN”", "“RTGS”", "“BI", "FAST”";
+            dataConfig.en = "yes";
+            dataConfig.id = "ya";
+        }
+
+    }
+}
+
+
+
+ 
--- modul8_1302220048/obj/Debug/net8.0/modul8_1302220048.AssemblyInfo.cs
+++ modul8_1302220048/obj/Debug/net8.0/modul8_1302220048.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("modul8_1302220048")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d322701a350a67392f57fc49483bd582c10f4f2c")]
 [assembly: System.Reflection.AssemblyProductAttribute("modul8_1302220048")]
 [assembly: System.Reflection.AssemblyTitleAttribute("modul8_1302220048")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
